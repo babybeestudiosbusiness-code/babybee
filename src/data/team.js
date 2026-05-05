cf44701908bae4157bcb52e559f63849fe6627de
BabyBee <babybeestudiosbusiness@gmail.com>
2026-05-05 11:41:41 +0600

    Update team.js

--- src/data/team.js
+++ src/data/team.js
@@ -1,32 +1,32 @@
 export const team = [
   {
-    name: "Sakib Salim",
-    role: "Founder & CEO",
+    name: "সাকিব সালিম",
+    role: "ফাউন্ডার",
     img: "/team/sakib.jpg",
   },
   {
-    name: "Mir Muntaka Newaz",
-    role: "Co-Founder",
+    name: "মীর মুনতাকা নেওয়াজ",
+    role: "কো-ফাউন্ডার",
     img: "/team/muntaka.jpg",
   },
   {
-    name: "Mahin Bhuiyan",
-    role: "Chief Operations Officer",
+    name: "মাহিন ভুঁইয়া",
+    role: "চিফ অপারেশন অফিসার",
     img: "/team/mahin.jpg",
   },
   {
-    name: "Shah Ariful Alam Shithil",
-    role: "Creative Director",
+    name: "শাহ আরিফুল আলম শিথিল",
+    role: "ক্রিয়েটিভ ডিরেক্টর",
     img: "/team/shithil.jpg",
   },
   {
-    name: "Maimun Aziz Zinan",
-    role: "Lead Generation Specialist",
+    name: "মাইমুন আজিজ জিনান",
+    role: "এক্সিকিউটিভ - প্ল্যানিং ডিভিশন",
     img: "/team/zinan.jpg",
   },
   {
-    name: "Rabiul Hasan Rumon",
-    role: "Senior Video Editor",
+    name: "রবিউল হাসান (রুমন)",
+    role: "সিনিয়র ভিডিও এডিটর",
     img: "/team/rabiul.jpg",
   },
 ];
